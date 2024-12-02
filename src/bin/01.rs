6b3f247d0d8d46148a76293882ba04d947bf18de
Davide Mantelli <davide.mantelli@docebo.com>
2024-12-02 16:15:59 +0100

    feat(day-01): solve day 01 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 use tinyjson::format;
 
 advent_of_code::solution!(1);
@@ -15,6 +17,31 @@ impl TwoCloumns {
         }
     }
 
+    fn parse(input: &str) -> Self {
+        input
+            .lines()
+            .map(|line| {
+                let entries: Vec<&str> = line.split_whitespace().collect();
+                if entries.len() != 2 {
+                    panic!("{}", format!("Invalid input: {}", line));
+                }
+                (
+                    entries[0]
+                        .parse::<u64>()
+                        .map_err(|_| format!("{} is not a positive integer", &entries[0]))
+                        .unwrap(),
+                    entries[1]
+                        .parse::<u64>()
+                        .map_err(|_| format!("{} is not a positive integer", &entries[1]))
+                        .unwrap(),
+                )
+            })
+            .fold(TwoCloumns::new(), |mut acc, curr| {
+                acc.add(curr.0, curr.1);
+                acc
+            })
+    }
+
     fn add(&mut self, left: u64, right: u64) {
         self.left.push(left);
         self.right.push(right);
@@ -25,7 +52,7 @@ impl TwoCloumns {
         self.right.sort();
     }
 
-    fn total_distance(&self) -> Option<u64> {
+    fn check_vec_lenth(&self) {
         if self.left.len() != self.right.len() {
             panic!(
                 "{}",
@@ -36,6 +63,10 @@ impl TwoCloumns {
                 )
             );
         }
+    }
+
+    fn total_distance(&self) -> Option<u64> {
+        self.check_vec_lenth();
         if self.left.len() == 0 {
             return None;
         }
@@ -43,42 +74,42 @@ impl TwoCloumns {
             self.left
                 .iter()
                 .zip(self.right.iter())
-                .map(|(a, b)| b.abs_diff(*a))
+                .map(|(left, right)| left.abs_diff(*right))
+                .sum(),
+        )
+    }
+
+    fn similaryty_score(&self) -> Option<u64> {
+        self.check_vec_lenth();
+        if self.left.len() == 0 {
+            return None;
+        }
+        let counters_by_id =
+            self.right
+                .iter()
+                .fold(HashMap::with_capacity(self.right.len()), |mut acc, curr| {
+                    acc.insert(curr, acc.get(curr).unwrap_or(&0) + 1);
+                    acc
+                });
+
+        Some(
+            self.left
+                .iter()
+                .map(|left| *left * *counters_by_id.get(left).unwrap_or(&0))
                 .sum(),
         )
     }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut two_columns_vec = input
-        .lines()
-        .map(|line| {
-            let entries: Vec<&str> = line.split_whitespace().collect();
-            if entries.len() != 2 {
-                panic!("{}", format!("Invalid input: {}", line));
-            }
-            (
-                entries[0]
-                    .parse::<u64>()
-                    .map_err(|_| format!("{} is not a positive integer", &entries[0]))
-                    .unwrap(),
-                entries[1]
-                    .parse::<u64>()
-                    .map_err(|_| format!("{} is not a positive integer", &entries[1]))
-                    .unwrap(),
-            )
-        })
-        .fold(TwoCloumns::new(), |mut acc, curr| {
-            acc.add(curr.0, curr.1);
-            acc
-        });
+    let mut two_columns_vec = TwoCloumns::parse(input);
 
     two_columns_vec.sort();
     two_columns_vec.total_distance()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    TwoCloumns::parse(input).similaryty_score()
 }
 
 #[cfg(test)]
@@ -94,6 +125,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
