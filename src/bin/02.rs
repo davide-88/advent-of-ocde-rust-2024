d0c03960c7caa7d46115ec38c05d488b31f30cf2
Davide Mantelli <davide.mantelli@docebo.com>
2024-12-02 18:25:32 +0100

    feat(day-02): solve day 02 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -14,7 +14,7 @@ impl ReportSafety {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn parse(input: &str) -> Vec<Vec<u32>> {
     let reports = input
         .lines()
         .map(|line| {
@@ -27,25 +27,48 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .collect::<Vec<u32>>()
         })
         .collect::<Vec<Vec<u32>>>();
+    reports
+}
+
+fn is_safe(report: &Vec<u32>) -> bool {
+    let diffs: Vec<i32> = report
+        .iter()
+        .zip(report.iter().skip(1))
+        .map(|(level, next_level)| *level as i32 - *next_level as i32)
+        .collect();
+    diffs.iter().all(|diff| &1 <= diff && diff <= &3)
+        || diffs.iter().all(|diff| &-3 <= diff && diff <= &-1)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let reports = parse(input);
+
+    Some(reports.iter().filter(|report| is_safe(report)).count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let reports = parse(input);
 
     Some(
         reports
             .iter()
             .filter(|report| {
-                let diffs: Vec<i32> = report
-                    .iter()
-                    .zip(report.iter().skip(1))
-                    .map(|(level, next_level)| *level as i32 - *next_level as i32)
-                    .collect();
-                diffs.iter().all(|diff| &1 <= diff && diff <= &3)
-                    || diffs.iter().all(|diff| &-3 <= diff && diff <= &-1)
+                let mut slice_index = 0;
+                loop {
+                    if slice_index == report.len() {
+                        return false;
+                    }
+                    let mut sub_array = Vec::with_capacity(report.len() - 1);
+                    sub_array.extend_from_slice(&report[..slice_index]);
+                    sub_array.extend_from_slice(&report[slice_index + 1..]);
+                    if is_safe(&sub_array) {
+                        return true;
+                    }
+                    slice_index += 1;
+                }
             })
             .count() as u32,
     )
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
@@ -61,6 +84,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
